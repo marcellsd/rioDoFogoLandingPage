254314dae221e29317d601177e80dfde9db38f13
Marilia Faria <mariliafs_@hotmail.com>
2024-10-04 12:39:25 -0300

    add logo altera circle-x-filled

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,13 @@ export default {
           escuro: '#1B2727',
         },
         amarelo: '#FF9E02'
+      },
+      screens: {
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px',
       }
     },
   },

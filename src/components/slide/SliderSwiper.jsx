1947c2d07ac93a9836484a59710b0bb84422479d
Marcell Santana de Deus <marcellsantana@gmail.com>
2025-06-08 16:48:12 -0300

    Fix gallery order

--- src/components/slide/SliderSwiper.jsx
+++ src/components/slide/SliderSwiper.jsx
@@ -7,16 +7,16 @@ import "./styles.css";
 
 
 const galeria = [
-  {id:0, href: "/galeria/", src: "/img10.jpg" },
-  {id:1, href: "/galeria/1", src: "/img2.jpg" },
-  {id:2, href: "/galeria/2", src: "/img3.jpg" },
-  {id:3, href: "/galeria/3", src: "/img5.jpg" },
-  {id:4, href: "/galeria/4", src: "/img7.jpg" },
-  {id:5, href: "/galeria/5", src: "/img1.jpg" },
-  {id:6, href: "/galeria/6", src: "/img4.jpg" },
-  {id:7, href: "/galeria/7", src: "/img6.jpg" },
-  {id:8, href: "/galeria/8", src: "/img8.png" },
-  {id:9, href: "/galeria/9", src: "/img9.jpg" },
+  {id:1, href: "/galeria/",    src: "/img10.jpg" },
+  {id:2, href: "/galeria/2",   src: "/img2.jpg" },
+  {id:3, href: "/galeria/3",   src: "/img3.jpg" },
+  {id:4, href: "/galeria/4",   src: "/img5.jpg" },
+  {id:5, href: "/galeria/5",   src: "/img7.jpg" },
+  {id:6, href: "/galeria/6",   src: "/img1.jpg" },
+  {id:7, href: "/galeria/7",   src: "/img4.jpg" },
+  {id:8, href: "/galeria/8",   src: "/img6.jpg" },
+  {id:9, href: "/galeria/9",   src: "/img8.jpg" },
+  {id:10, href: "/galeria/10", src: "/img9.jpg" },
 ];
 
 const SliderSwiper = () => {
